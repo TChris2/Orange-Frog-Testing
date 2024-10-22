cab1ce03e0454859cabac48d70c98b9ceff74f07
nilsenlukas <lukas.nilsen2021lax@gmail.com>
2024-10-22 17:48:00 -0400

    updated ui from class recomendations

--- server/mongo.js
+++ server/mongo.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@orangefrog.xmt6e.mongodb.net/?retryWrites=true&w=majority&appName=OrangeFrog`;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
     .then(() => {
         console.log("Connected to MongoDB");
     })
@@ -12,19 +11,21 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.error("Failed to connect to MongoDB");
     });
 
-    const userSchema = new mongoose.Schema({
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true }, 
-        address: { type: String },
-        dob: { type: Date },
-        temporaryPassword: { type: Boolean, default: true } 
-    });
-    
-    const userCollection = mongoose.model('userCollection', userSchema);
-    
-    const collection = {
-        userCollection
-    };
-    
-    module.exports = collection;
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: String },
+    dob: { type: Date },
+    allergies: { type: String },
+    extraComments: { type: String },
+    temporaryPassword: { type: Boolean, default: true }
+});
+
+const userCollection = mongoose.model('userCollection', userSchema);
+
+const collection = {
+    userCollection 
+};
+
+module.exports = collection;
