82c7617c0ad50e766e7c76e63227b09333f9535a
nilsenlukas <lukas.nilsen2021lax@gmail.com>
2024-10-23 15:22:18 -0400

    start of even page (having issues with fetching/adding data to database)

--- server/index.js
+++ server/index.js
@@ -70,6 +70,33 @@ app.post('/create-user', async (req, res) => {
     }
 });
 
+//
+app.post('/create-event', async (req, res) => {
+    const { eventName, eventLoadIn, eventLoadOut, eventLocation, eventHours, eventDescription } = req.body;
+
+    if (!eventName || !eventDate || !eventLocation) {
+        return res.status(400).json({ message: 'Event Name, Date, and Location are required' });
+    }
+
+    try {
+        const newEvent = new eventCollection({
+            eventName,
+            eventLoadIn,
+            eventLoadOut,
+            eventLocation,
+            eventHours, 
+            eventDescription
+        });
+    
+        await newEvent.save();
+        res.status(200).json({ message: 'Event created successfully' });
+    } catch (error) {
+        console.error('Error creating event:', error);
+        res.status(500).json({ message: 'Error creating event' });
+    }
+});
+
+
 // User login and password
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
